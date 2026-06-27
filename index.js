ac98c477c810e8d684769887d053d82d18636a1e
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-27 20:33:31 +0630

    step up open ai in js code

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const inputEl = document.getElementById('')
 const radioEl = document.querySelector('.radio')
+import OpenAI from "openai"
 
-document.querySelector('form').addEventListener('submit', function(e){
+const openai = new OpenAI({
+    apiKey:"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",
+    baseURL:"https://api.groq.com/openai/v1"
+})
+
+const messages=[
+    {
+    role: "user",
+    content:`${inputEl.value}`
+}
+]
+
+document.querySelector('form').addEventListener('submit', async function(e){
     e.preventDefault()
+
     if(inputEl.value && radioEl.value){
         
+    const response = await openai.chat.completions.create({
+      model : "openai/gpt-oss-120b",
+      messages 
+    })
     }
 })
