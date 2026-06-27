57b6ff69f594e9a53ca497bda089b3bbfe9eed77
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-27 20:19:05 +0630

    installed open ai

---
+++ index.js
@@ -0,0 +1,9 @@
+const inputEl = document.getElementById('')
+const radioEl = document.querySelector('.radio')
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault()
+    if(inputEl.value && radioEl.value){
+        
+    }
+})
