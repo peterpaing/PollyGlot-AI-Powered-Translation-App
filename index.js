5c67395307fa5d016862854ada71d1fd3119e275
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-27 21:01:04 +0630

    stepup open ai

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const inputEl = document.getElementById('')
-const radioEl = document.querySelector('.radio')
 import OpenAI from "openai"
+import 'dotenv/config'
+
+const inputEl = document.getElementById('input-text')
+const radioEl = document.querySelector('.radio-select')
 
 const openai = new OpenAI({
-    apiKey:"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",
-    baseURL:"https://api.groq.com/openai/v1",
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1",
     dangerouslyAllowBrowser: true
 })
-
-const messages=[
-    {
-    role: "user",
-    content:`${inputEl.value}`
-}
-]
 
 document.querySelector('form').addEventListener('submit', async function(e){
     e.preventDefault()
 
+    
+    const messages = [
+        {
+            role: "user",
+            content: inputEl.value
+        }
+    ]
+
     if(inputEl.value && radioEl.value){
-        
-    const response = await openai.chat.completions.create({
-      model : "openai/gpt-oss-120b",
-      messages 
-    })
+        try {
+            
+            const response = await openai.chat.completions.create({
+                model: "openai/gpt-oss-20b", 
+                messages: messages 
+            })
+            
+            console.log(response.choices[0].message.content)
+        } catch (error) {
+            console.error("API Error:", error)
+        }
     }
 })
